e923a3e0e6f43768b00ff1b1b05c0c7139bd652b
CesarPmDev17 <cesar.17.29.10@gmail.com>
2024-06-10 19:51:42 -0500

    Commit Inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'capacitor-demo',
+  webDir: 'dist/capacitor-demo'
+};
+
+export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Geolocation, PermissionStatus, GeolocationPluginPermissions} from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'capacitor-demo';
+  public latitude: number = 0;
+  public permissionStatus: PermissionStatus = { location: 'prompt', coarseLocation: 'prompt' };
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const coords = Geolocation.getCurrentPosition();
+    if(this.isiOS()){
+      console.log('Es Ios');
+    }
+    if(this.isAndroid()){
+      console.log('Es Android');
+    }
+    console.log(coords);
+    //this.requestLocationPermissions(); // Solicitar permisos cuando se inicie el componente
+    //this.getCurrentPosition();
+  }
+
+  async requestLocationPermissions() {
+    try {
+      this.permissionStatus = await this.requestPermissions(); // Solicitar permisos y actualizar el estado
+    } catch (error) {
+      console.error('Error al solicitar permisos de geolocalización:', error);
+    }
+  }
+
+  async requestPermissions(permissions?: GeolocationPluginPermissions): Promise<PermissionStatus> {
+    if (Geolocation.requestPermissions) {
+      return Geolocation.requestPermissions(permissions);
+    } else {
+      console.log('Geolocation.requestPermissions no está disponible en este entorno.');
+      throw new Error('Geolocation.requestPermissions no está disponible en este entorno.');
+    }
+  }
+
+  async getCurrentPosition() {
+    try {
+    
+      if (this.permissionStatus.location === 'granted') {
+        const coordinates = await Geolocation.getCurrentPosition();
+        console.log('Current position:', coordinates.coords.latitude);
+        this.latitude = coordinates.coords.latitude;
+      } else {
+        console.warn('No se tienen los permisos necesarios para acceder a la ubicación.');
+      }
+    } catch (error) {
+      console.error('Error al obtener la posición actual:', error);
+    }
+  }
+  isiOS() {
+    return /iPad|iPhone|iPod/.test(window.navigator.userAgent);
+  }
+  
+  // Función para verificar si el usuario está en un dispositivo Android
+   isAndroid() {
+    return /Android/.test(window.navigator.userAgent);
+  }
+  
+  
 }
